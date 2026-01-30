b49e8a5feec6ea53805c904049252f799b9f9ca9
Kaiutzi21 <kaiutzi21@googlemail.com>
2026-01-30 21:33:18 +0100

    Breathing effect on enemy slots

--- scripts/enemy_slot.gd
+++ scripts/enemy_slot.gd
@@ -9,6 +9,17 @@ func _ready() -> void:
 	var tween = create_tween()
 	scale = Vector2.ZERO
 	tween.tween_property(self, "scale", Vector2(1,1), 0.5).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+	tween.finished.connect(self.breathing_effect)
+
+func breathing_effect():
+	var tween = create_tween().set_loops()
+	var duration = randf_range(1.5, 2.2)
+	tween.tween_property(sprite, "scale", Vector2(1.04, 0.96), duration)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(sprite, "scale", Vector2(1.0, 1.0), duration)\
+		.set_trans(Tween.TRANS_SINE)\
+		.set_ease(Tween.EASE_IN_OUT)
 
 func _on_button_pressed() -> void:
 	clicked.emit(self)
