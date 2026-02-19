a34b843b57b32160008503d188ee68dfa9a9ce51
Kaiutzi21 <kaiutzi21@googlemail.com>
2026-02-19 16:49:06 +0100

    Implement multi-hit loop and increase hit delay
    Modify DamageEffect.gd to apply damage hit_count times (looping through targets for each hit). Increase Schattenboxen skill's delay_between_hits from 0.3 to 1.2 to space out the multi-hits. Update Talahons.tres to reference the resaved Schattenboxen resource (ext_resource id changed). These changes make the Schattenboxen skill perform multiple sequential hits with a longer pause between them.

--- scripts/ResourceSkripts/SkillData/Modules/DamageEffect.gd
+++ scripts/ResourceSkripts/SkillData/Modules/DamageEffect.gd
@@ -11,18 +11,19 @@ class_name DamageEffect
 @export var delay_between_hits: float = 0.0
 
 func apply(user, targets: Array, context):
-	for target in targets:
-		if not target.is_alive():
-			continue
-		var attacker_stat = user.data.get(scaling_stat)
-		var raw_damage = base_damage + (attacker_stat * scaling_factor)
-		if can_crit:
-			var crit_chance = user.data.luck * 0.5
-			if randf() * 100 < crit_chance:
-				raw_damage *= crit_multiplier
-				context.log("KRITISCHER TREFFER!", Color.YELLOW)
-		var final_damage = calculate_damage_after_defense(raw_damage, target)
-		context.apply_damage(user, target, final_damage)
+	for i in hit_count:
+		for target in targets:
+			if not target.is_alive():
+				continue
+			var attacker_stat = user.data.get(scaling_stat)
+			var raw_damage = base_damage + (attacker_stat * scaling_factor)
+			if can_crit:
+				var crit_chance = user.data.luck * 0.5
+				if randf() * 100 < crit_chance:
+					raw_damage *= crit_multiplier
+					context.log("KRITISCHER TREFFER!", Color.YELLOW)
+			var final_damage = calculate_damage_after_defense(raw_damage, target)
+			context.apply_damage(user, target, final_damage)
 
 	if delay_between_hits > 0: 
 		await context.manager.get_tree().create_timer(delay_between_hits).timeout
